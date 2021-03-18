b019f533c9a68da72c0cd2ff8bfe17dc93f19000
thiles03 <thiles03@gmail.com>
2021-03-18 12:40:40 +0000

    Mail form

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "thiles03@gmail.com";
+
+$subject = "Message from Garden Umpire website.";
+$txt ="Name: ". $name . "\r\n  Email: " . $email . "\r\n Message: " . $message;
+
+$headers = "From: noreply@gardenumpire.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
